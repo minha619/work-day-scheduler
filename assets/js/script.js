4c70c44f60c78c1865b26b6fd6325b8f49fadd10
Your Name <minhakim@mins-mbp.home>
2021-09-26 22:16:44 -0400

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -51,3 +51,42 @@ function saveOnclick(event) {
     localStorage.setItem(key, value)
 }
 
+setInterval(function () {
+    var now = new Date();
+    var past = moment(now).add(-1, 'seconds').toDate();
+    //console.log(past, now)
+
+    if (past.getHours() !== now.getHours()) {
+        var rows = document.querySelectorAll('.row');
+        for (var i = 0; i < rows.length; i++) {
+            rows[i].querySelector('.textarea').classList.remove('past');
+            rows[i].querySelector('.textarea').classList.remove('present');
+            rows[i].querySelector('.textarea').classList.remove('future');
+
+            var time = rows[i].querySelector('.hours').innerHTML
+
+            var currentTime = moment(time, "hh:mm A").toDate();
+            var endTime = moment(currentTime).add(59, 'minutes').toDate();
+
+            if (currentTime.getHours() < now.getHours()) {
+                // $(rows[i]).find('.textarea').addClass("past");
+                rows[i].querySelector('.textarea').classList.add('past');
+            }
+            else if (endTime.getHours() === now.getHours() && now.getMinutes() <= endTime.getMinutes()) {
+                rows[i].querySelector('.textarea').classList.add('present');
+            }
+            else {
+                rows[i].querySelector('.textarea').classList.add('future');
+                //console.log('future')
+            }
+
+            var key = time + "-note";
+            var value = localStorage.getItem(key);
+
+            //console.log(key, value)
+            if (value !== null) {
+                rows[i].querySelector('.textarea').value = value
+            }
+        }
+    }
+}, 1000);
